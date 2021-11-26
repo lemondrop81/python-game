5eeac0db29314800071557d3d480c73576da7ce1
lemondrop81 <hacker814@hotmail.com>
2021-11-26 08:26:15 -0600

    Inital prompts and call the start function

---
+++ adventure.py
@@ -0,0 +1,9 @@
+def start():
+    # give the inital prompts.
+    print("\nYou are standing in a dark and creepy hallway.")
+    print("There is a door to your left, another to the right and a hallway in front of you. Which way do you go? (l, r or c)")
+
+
+
+# starting the game
+start()
