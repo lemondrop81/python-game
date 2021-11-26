dc3511c821d8d423e820ae1ba9fdd2abff3aaa40
lemondrop81 <hacker814@hotmail.com>
2021-11-26 09:06:17 -0600

    Import trophy room

--- spider_room.py
+++ spider_room.py
@@ -1,5 +1,8 @@
 #import the function defined in game_over.py
-from game_over import *
+from game_over import game_over
+
+# import the function defined in trophy_room.py
+from trophy_room import trophy_room
 
 # spider room
 def spider_room():
