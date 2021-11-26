db8a6e99c8abe96706c5844e5781411660c7690a
lemondrop81 <hacker814@hotmail.com>
2021-11-26 08:31:19 -0600

    Get the player input

--- adventure.py
+++ adventure.py
@@ -3,6 +3,11 @@ def start():
     print("\nYou are standing in a dark and creepy hallway.")
     print("There is a door to your left, another to the right and a hallway in front of you. Which way do you go? (l, r or c)")
 
+    # convert the player's input() to lower_case
+    answer = input(">").lower()
+
+    print(answer)
+
 
 
 # starting the game
