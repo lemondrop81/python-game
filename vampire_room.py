de329ce3e563c18a100a5e248b2e6be7fcf18b26
lemondrop81 <hacker814@hotmail.com>
2021-11-26 09:23:14 -0600

    vampire_room prompts

--- vampire_room.py
+++ vampire_room.py
@@ -2,3 +2,14 @@
 from game_over import game_over
 
 # vampire room
+def vampire_room():
+    # give the prompts
+    prompt =  """
+            You spot a vampire sitting in a library.
+            The vampire invites you to sit down and join him in reading
+            What would you do (1, 2 or 3)
+            (1). Sit down and join the vampire.
+            (2). Attack the vampire with a wooden stake.
+            (3). Turn around and head back through the only door in the room """
+    
+    print(prompt)
