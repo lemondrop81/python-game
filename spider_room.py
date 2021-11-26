d18733b66f445928a595f3980189e5f15cce9e31
lemondrop81 <hacker814@hotmail.com>
2021-11-26 08:40:53 -0600

    spider_room prompt

---
+++ spider_room.py
@@ -0,0 +1,12 @@
+# spider room
+def spider_room():
+    # give the prompts
+    a =  """
+            \nThere is a spider here.
+            Behind the spider is another door. 
+            The spider is eating goat
+            What would you do (1 or 2)
+            (1) Take the goat.
+            (2) Taunt the spider. 
+        """
+    
