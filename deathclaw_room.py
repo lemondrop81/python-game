7b64d5a1ce04544b6896da085ae7852ef9a7c66e
lemondrop81 <hacker814@hotmail.com>
2021-11-26 09:10:41 -0600

    deathclaw_room prompts

--- deathclaw_room.py
+++ deathclaw_room.py
@@ -5,3 +5,13 @@ from game_over import game_over
 from trophy_room import trophy_room
 
 # deathclaw room
+def deathclaw_room():
+    # give the prompts
+    prompt =  """
+            You enter the room and see a sleeping deathclaw
+            Behind the deathclaw is another door. 
+            What would you do (1 or 2)
+            (1) Go through the door silently.
+            (2) Kill the deathclaw and show your bravery. """
+    
+    print(prompt)
