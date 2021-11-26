7e3838b223c698bb578fe51d74f68e882c4fcd54
lemondrop81 <hacker814@hotmail.com>
2021-11-26 09:08:21 -0600

    Create deathclaw_room

---
+++ deathclaw_room.py
@@ -0,0 +1,7 @@
+#import the function defined in game_over.py
+from game_over import game_over
+
+# import the function defined in trophy_room.py
+from trophy_room import trophy_room
+
+# deathclaw room
