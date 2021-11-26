7d5c1076b75d98333128732bd3e6380019b059ed
lemondrop81 <hacker814@hotmail.com>
2021-11-26 08:58:05 -0600

    trophy_room prompt

--- trophy_room.py
+++ trophy_room.py
@@ -1,1 +1,11 @@
 #trophy room
+def trophy_room():
+    # give the prompts
+    prompt =  """
+            You are now in a room filled with valuable trophies!
+            There is a gold jewel encrusted door as well!
+            What would you do? (1 or 2)
+            (1). Take some trophies and go through the door.
+            (2). Just go through the door. """
+    
+    print(prompt)
