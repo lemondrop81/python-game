973ff650d90d9edf562b26f29eac6cf29410d2be
lemondrop81 <hacker814@hotmail.com>
2021-11-26 08:33:01 -0600

    Call the spider room

--- adventure.py
+++ adventure.py
@@ -1,12 +1,15 @@
+
 def start():
     # give the inital prompts.
     print("\nYou are standing in a dark and creepy hallway.")
-    print("There is a door to your left, another to the right and a hallway in front of you. Which way do you go? (l, r or c)")
+    print("There is a door to your left, another to the right and a hallway straight ahead. Which way do you go? (l, r or s)")
 
     # convert the player's input() to lower_case
     answer = input(">").lower()
 
-    print(answer)
+    if "l" in answer:
+        # if the player typed "left" or "l" lead him to the spider_room()
+        spider_room()
 
 
 
